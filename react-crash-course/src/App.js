d8f9ae3d661b476dd7ef447909a3613bbc594ad4
Bengt Sjögren <Bengan92@gmail.com>
2023-08-28 17:44:08 +0200

    useffect and useState

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -1,26 +1,18 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
-// arrow way to define function
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  )
-  
-}
 
 function App() {
-  const name = 'Ben';
-  const isNameShowing = true;
-
+  const [counter, setCounter] = useState(0); // hooks start with 'use¨ in func names
+  useEffect(() => {
+    setCounter(100);
+  }, []); // can call code whenever variavble changes
+  // callback function onClick
   return (
     <div className="App">
-    
-    <Person name={'Ben'} lastName={'Doe'} age={30}/>
-    <Person name='Jane' lastName='Doe' age={30} />
+      <button onClick={() => setCounter((prevCount) => prevCount-1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount+1)}>+</button>
     </div>
   );
 }
