f8b625700ac7f7eba0d47179efdd9d8dda241183
Bengt Sjögren <Bengan92@gmail.com>
2023-09-04 21:02:32 +0200

    add share-prompts tutorial example

--- share-prompts/components/Nav.jsx
+++ share-prompts/components/Nav.jsx
@@ -6,12 +6,12 @@ import {signIn, signOut, getProviders, useSession } from "next-auth/react";
 
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const {data: session} = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
 
             setProviders(response);
@@ -30,9 +30,10 @@ const Nav = () => {
                 alt="prompt-app" />
                 <p className="logo_text">Promptopia</p>
             </Link>
+
             {/* Desktop Navigation */}
             <div className="sm:flex hidden">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex gap-3 md:gap-5">
                         <Link href="/create-prompt"
                         className="black_btn">
@@ -74,7 +75,7 @@ const Nav = () => {
             </div>
             {/* Mobile navigation */}
             <div className="sm:hidden flex relative">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex">
                         <Image
                             src="/assets/images/logo.svg"
